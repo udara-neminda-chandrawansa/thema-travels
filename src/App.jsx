644c427f7ea143622fe4c09a0a24325acd55e15c
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-12-16 18:29:15 +0530

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import round from "/favicon.png";
 import brown_bottom from "./assets/home/brown-bottom.png";
 import side_round from "./assets/home/side-round.png";
 import siigiriya_main from "./assets/home/sigiriya_main.png";
@@ -6,15 +7,23 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="flex h-dvh justify-between items-center flex-col bg-cover bg-bottom" style={{ backgroundImage: `url(${siigiriya_main})` }}>
+      <div className="flex flex-col items-center justify-between bg-bottom bg-cover h-dvh" style={{ backgroundImage: `url(${siigiriya_main})` }}>
         <div className="text-center h-1/3 pt-[100px] z-10 text-white">
           <p className="angelina text-[900%] max-md:text-[700%]">Thema</p>
           <p className="uppercase text-8xl max-sm:text-6xl">Travels</p>
-          <p className="uppercase text-3xl max-sm:text-xl font-bold">
+          <p className="text-3xl font-bold uppercase max-sm:text-xl">
             where tradition meets exploration
           </p>
+          <div className="w-full h-[30px] gap-6 flex items-center justify-center">
+            <span className="h-[2px] bg-white w-1/3 opacity-70"></span>
+            <img src={round} className="h-full" />
+            <div className="flex flex-col h-[10px] gap-1 w-1/3 mb-1 opacity-80">
+              <span className="h-[2px] bg-white"></span>
+              <span className="h-[2px] w-2/3 bg-amber-900"></span>
+            </div>
+          </div>
         </div>
-        <div className="w-full h-1/3 relative bg-cover bg-bottom opacity-80" style={{ backgroundImage: `url(${brown_bottom})` }}>
+        <div className="relative w-full bg-bottom bg-cover h-1/3 opacity-80" style={{ backgroundImage: `url(${brown_bottom})` }}>
           <img src={side_round} alt="side-round" className="absolute bottom-0" />
         </div>
       </div>
