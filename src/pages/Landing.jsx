a1fc94bb185cfa209222c66e5ee898933a167c65
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-12-17 12:14:25 +0530

    responsive fixes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -25,17 +25,19 @@ function Landing() {
         </div>
       </div>
       {/*welcome*/}
-      <div className="relative px-12 py-6 max-md:px-6 h-[60dvh] flex flex-col items-center">
-        <div className="absolute flex justify-center w-5/6 h-full px-12 py-6 bg-white -top-10 rounded-2xl max-md:px-6">
-          <div className="flex flex-col justify-center w-1/3 gap-6">
+      <div className="relative px-12 py-6 max-md:px-6 h-[60dvh] flex flex-col items-center max-lg:h-fit">
+        <div className="absolute flex justify-center w-5/6 h-full p-12 bg-white -top-10 rounded-2xl max-md:p-6 max-lg:flex-col max-lg:relative max-lg:top-0 max-lg:gap-6 max-lg:w-full max-lg:shadow-lg max-sm:shadow-none max-sm:rounded-none max-sm:p-0">
+        {/*left side*/}
+          <div className="flex flex-col justify-center w-1/3 gap-6 max-sm:gap-3 max-lg:w-full">
             <p className="text-xl arapay">A Warm</p>
             <p className="text-6xl text-[#700302] arapay">Welcome</p>
             <p className="text-xl arapay">To Thema Travels </p>
             <p className="text-6xl text-[#700302] arapay">SRI LANKA</p>
           </div>
-          <div className="relative flex items-center justify-center w-2/3 px-12 py-6 max-md:px-6">
-            <p className="absolute poltawski-nowy -top-1 left-5 text-[#700302]">“</p>
-            <p className="h-fit">
+          {/*right side*/}
+          <div className="relative flex items-center justify-center w-2/3 px-12 py-6 max-lg:w-full max-md:px-6 max-sm:p-0">
+            <p className="absolute poltawski-nowy top-3 left-2 text-[#700302] max-sm:hidden">“</p>
+            <p className="h-fit max-md:px-6 max-sm:px-0">
               Dear Traveler's, On behalf of the entire team at Travels Sri
               Lanka, I would like to extend a warm welcome to you. It is with
               great pride and joy that we invite you to embark on an
@@ -53,7 +55,7 @@ function Landing() {
               step of the way. Thank you for choosing Travels Sri Lanka. We look
               forward to being a part of your incredible journey. Warm regards,
             </p>
-            <p className="absolute poltawski-nowy -bottom-1 right-5 place-content-end text-[#700302]">”</p>
+            <p className="absolute poltawski-nowy bottom-3 right-2 place-content-end text-[#700302] max-sm:hidden">”</p>
           </div>
         </div>
       </div>
